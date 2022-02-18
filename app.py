80dbd265aadf4810680861cd17ea06b3932fe27c
Georgton Ramos <99138157+georgtonramos@users.noreply.github.com>
2022-02-18 18:53:19 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+chrome_options = webdriver.ChromeOptions()
+navegador = webdriver.Chrome(chrome_options=chrome_options)
+
+# Accessing the website
+navegador.get('https://www.google.com'),
+time.sleep(10),
+# Writing Search Term
+navegador.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('search term'),
+time.sleep(2),
+# Click Search
+navegador.find_element(By.XPATH, '/ html / body / div[1] / div[3] / form / div[1] / div[1] / div[3] / center / input[1]').click(),
+time.sleep(10),
+webdriver.Chrome.quit(navegador)
